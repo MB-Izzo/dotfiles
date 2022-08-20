01e8bf21dada17b23e7b29e25e97ae9d3829deff
Mathieu Bouchonnet <matbouch18@gmail.com>
2022-08-20 21:59:40 +0200

    catpuccin theme

--- nvim/.config/nvim/lua/user/closetag.lua
+++ nvim/.config/nvim/lua/user/closetag.lua
@@ -1,5 +1,8 @@
-vim.g.closetag_filetypes = "js,jsx,html,xhtml"
+vim.g.closetag_filetypes = "js,jsx,html,xhtml,ts,tsx"
+vim.g.closetag_xhtml_filetypes = 'xhtml,jsx,tsx'
 vim.g.closetag_filenames = "*.html,*.jsx,*.js,*.ts,*.tsx,*.vue"
+vim.g.closetag_xhtml_filenames = "*.xhtml,*.jsx,*.tsx"
+vim.g.closetag_regions = { typescriptjsx = 'jsxRegion,tsxRegion', javascripttsx = 'jsxRegion'}
 vim.g.closetag_shortcut = '>'
 
 
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -91,6 +91,7 @@ return packer.startup(function(use)
     use 'kyazdani42/nvim-web-devicons'
     use 'kyazdani42/nvim-tree.lua'
     use 'segeljakt/vim-silicon'
+    use 'catppuccin/nvim'
     if PACKER_BOOTSTRAP then
         require("packer").sync()
     end
